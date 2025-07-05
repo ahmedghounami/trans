ca84c14406fbf43c3bf2080686fb8e45be2fbabe
mkibous <mehdikib321@gmail.com>
2025-07-05 16:42:58 +0100

    game logic in server

--- client/app/game/page.tsx
+++ client/app/game/page.tsx
@@ -1,128 +1,31 @@
 'use client';
 import Image from "next/image";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Game()
 {
     const [Positions, setPositions] = useState({})
-    const positionsRef = useRef(Positions);
-    const bootrange = 80;
-    useEffect(()=>{
-const socket = new WebSocket('ws://localhost:9090');
-    socket.addEventListener('open', ()=>{
-        socket.send('getpositions')
-    })
-    socket.onmessage = (event)=>{
-        const data = JSON.parse(event.data)
-        console.log(data);
-        setPositions(data)
-    }},[])
-    // useEffect(()=>{
-    // async function start()
-    // {
-    //     fetch("http://localhost:8080/game")
-    //     .then(async(datajson) => {
-    //         const data = await datajson.json()
-    //         setPositions(data)
-            
-    //     })
-    //     .catch((err)=>console.log(err))
-
-    // }
-    // start();
-
-    // },[])
-    
-    // const [keysPressed, setKeysPressed] = useState<Record<string, boolean>>({});
-    const keysPressed = useRef({})
-    useEffect(()=>{
-        positionsRef.current = Positions;
-    },[Positions])
     useEffect(()=>
     {
+        const socket = new WebSocket('ws://localhost:9090');
+        socket.addEventListener('open', ()=>{
+            socket.send(JSON.stringify({type:'getpositions'}))
+        })
+        socket.onmessage = (event)=>{
+            const data = JSON.parse(event.data)
+            setPositions(data)
+        }
         function keydown(event: Event){
-        keysPressed.current[event.key] = true;
+        socket.send(JSON.stringify({type:'keydown', key:event.key}))
         }
         function keyup(event: Event){
-        keysPressed.current[event.key] = false;
+        socket.send(JSON.stringify({type:'keyup', key:event.key}))
     }
                 
         
         document.addEventListener('keydown', keydown)
         document.addEventListener('keyup', keyup)
     },[])
-      useEffect(() => {
-        setTimeout(()=>{
-            const intervalid = setInterval(()=>{
-            
-            
-            // console.log(keysPressed);
-            let newpositions = positionsRef.current;
-            let vx = Math.cos(newpositions.angle * (Math.PI / 180)) * newpositions.speed;
-            let vy = Math.sin(newpositions.angle * (Math.PI / 180)) * newpositions.speed;
-            vx *= newpositions.direction;
-            vy *= newpositions.direction;
-            
-
-
-            if(keysPressed.current["w"] &&( newpositions.p1 - 10)-2.5 >= 0)     
-                newpositions.p1=newpositions.p1-2.5
-            if(keysPressed.current["s"] &&  (10 + newpositions.p1) + 2.5 <= 100 )
-                newpositions.p1=newpositions.p1+2.5
-            // if(keysPressed.current["ArrowUp"] && newpositions.p2 - 2.5 - 10 >= 0)
-            //     newpositions.p2=newpositions.p2-2.5
-            // if(keysPressed.current["ArrowDown"] && (10 + newpositions.p2) + 2.5 <= 100 )
-            //     newpositions.p2=newpositions.p2+2.5
-            if(newpositions.bally > newpositions.p2 +5 && newpositions.ballx > newpositions.bootrange   && (10 + newpositions.p2) + 2.5 <= 100 )
-                newpositions.p2=newpositions.p2+2.5
-            else if(newpositions.bally < newpositions.p2 -5 && newpositions.ballx > newpositions.bootrange  && ( newpositions.p2 - 10) - 2.5 >= 0 )
-                newpositions.p2=newpositions.p2-2.5
-            if( newpositions.ballx <= 100  && newpositions.ballx + vx >= 0 )
-            {
-                
-                if(((newpositions.bally >= newpositions.p2 - 10 && newpositions.bally <= newpositions.p2 + 10) && newpositions.ballx + vx > 96))
-                        {
-                        let diff = (newpositions.bally - newpositions.p2) / 10;
-                        if(!diff)
-                            diff = 0.02
-                        newpositions.direction = -1;
-                        newpositions.angle = diff *(-75);
-                        if(newpositions.speed < 3)
-                            newpositions.speed += 0.05;
-                        }
-                else if(((newpositions.bally >= newpositions.p1 - 10 && newpositions.bally <= newpositions.p1 + 10) && newpositions.ballx + vx  < 4) && newpositions.direction == -1)
-                    {
-                        let diff = (newpositions.bally - newpositions.p1) / 10;
-                        if(!diff)
-                            diff++;
-                        newpositions.direction = 1;
-                        newpositions.angle = diff *(75)
-                        if(newpositions.speed < 3)
-                            newpositions.speed += 0.05;
-                    }
-                if((newpositions.bally + vy  <= 2 || newpositions.bally + vy >= 98) && !newpositions.directionchanged)
-                    {
-                        if(!newpositions.angle)
-                            newpositions.angle -= 5;
-                        newpositions.angle *= -1 ;
-                        newpositions.directionchanged = true;
-                    }
-                else if((newpositions.bally + vy >  2 && newpositions.bally + vy < 98)) 
-                    newpositions.directionchanged = false;
-                newpositions = {...newpositions, ballx:(newpositions.ballx+vx), bally:(newpositions.bally+vy)}
-                
-            }
-            else{
-                newpositions = {...newpositions, ballx:50, bally:50, angle:0, speed:1}
-            }
-            setPositions(newpositions)
-            // if(Positions.ballx == 100)
-            //     setPositions((oldposition) => {return({...oldposition, ballx:50})})
-
-        }, 20)},1000)
-        }, []);
-        // console.log(Positions);
-        
     
     return<div className="bg-gray-400/30 flex justify-center items-center z-50 backdrop-blur-sm absolute top-0 bottom-0 left-0 right-0   ">
         <div className="flex  flex-col gap-5 w-2/3">
--- server/game.js
+++ server/game.js
@@ -1,21 +1,86 @@
-let positions = {p1:50, p2:50, ballx:50, bally:50, angle:0, vx:0, vy:0, direction:1, directionchanged:false, speed:1, bootrange:80}
+let positions = {p1:50, p2:50, ballx:50, bally:50, angle:0, vx:0, vy:0, direction:1, directionchanged:false, speed:1, bootrange:70}
 // wsServer.js
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port:  9090});
-
-wss.on('connection', (ws) => {
+const ws = new WebSocket.Server({ port:  9090});
+const keysPressed = {};
+ws.on('connection', (ws) => {
   console.log('Client connected');
 
-//   ws.on('keydown', (key) => {
-//     console.log('Received:', key);
-//     ws.send(`Server got your key: ${key}`);
-//   });
   ws.on('message', (msg) => {
-    if(msg == 'getpositions' ){
+    const data = JSON.parse(msg);
+    if(data.type == 'getpositions' ){
     console.log('getpositions');
     ws.send(JSON.stringify(positions));}
+    if(data.type == "keydown")
+    {
+      keysPressed[data.key] = true;
+    }
+    if(data.type == "keyup")
+    {
+      keysPressed[data.key] = false;
+    }
+    
+
   });
+  setInterval(()=>{
+  let vx = Math.cos(positions.angle * (Math.PI / 180)) * positions.speed;
+  let vy = Math.sin(positions.angle * (Math.PI / 180)) * positions.speed;
+  vx *= positions.direction;
+  vy *= positions.direction;
+  
+  if(keysPressed["w"] &&( positions.p1 - 10)-2.5 >= 0)     
+      positions.p1=positions.p1-2.5
+  if(keysPressed["s"] &&  (10 + positions.p1) + 2.5 <= 100 )
+      positions.p1=positions.p1+2.5
+  // if(keysPressed["ArrowUp"] && positions.p2 - 2.5 - 10 >= 0)
+  //     positions.p2=positions.p2-2.5
+  // if(keysPressed["ArrowDown"] && (10 + positions.p2) + 2.5 <= 100 )
+  //     positions.p2=positions.p2+2.5
+  if(positions.bally > positions.p2 +5 && positions.ballx > positions.bootrange   && (10 + positions.p2) + 2.5 <= 100 )
+      positions.p2=positions.p2+2.5
+  else if(positions.bally < positions.p2 -5 && positions.ballx > positions.bootrange  && ( positions.p2 - 10) - 2.5 >= 0 )
+      positions.p2=positions.p2-2.5
+  if( positions.ballx <= 100  && positions.ballx + vx >= 0 )
+  {
+      
+      if(((positions.bally >= positions.p2 - 10 && positions.bally <= positions.p2 + 10) && positions.ballx + vx > 96))
+              {
+              let diff = (positions.bally - positions.p2) / 10;
+              if(!diff)
+                  diff = 0.02
+              positions.direction = -1;
+              positions.angle = diff *(-75);
+              if(positions.speed < 3)
+                  positions.speed += 0.05;
+              }
+      else if(((positions.bally >= positions.p1 - 10 && positions.bally <= positions.p1 + 10) && positions.ballx + vx  < 4) && positions.direction == -1)
+          {
+              let diff = (positions.bally - positions.p1) / 10;
+              if(!diff)
+                  diff++;
+              positions.direction = 1;
+              positions.angle = diff *(75)
+              if(positions.speed < 3)
+                  positions.speed += 0.05;
+          }
+      if((positions.bally + vy  <= 2 || positions.bally + vy >= 98) && !positions.directionchanged)
+          {
+              if(!positions.angle)
+                  positions.angle -= 5;
+              positions.angle *= -1 ;
+              positions.directionchanged = true;
+          }
+      else if((positions.bally + vy >  2 && positions.bally + vy < 98)) 
+          positions.directionchanged = false;
+      positions = {...positions, ballx:(positions.ballx+vx), bally:(positions.bally+vy)}
+      
+  }
+  else{
+      positions = {...positions, ballx:50, bally:50, angle:0, speed:1}
+  }
+  ws.send(JSON.stringify(positions))
+  }, 20)
 
   ws.send(JSON.stringify({ type: 'welcome', message: 'Welcome to the game' }));
 });
