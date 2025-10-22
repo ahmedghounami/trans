36295ede232e6bef662a248e53b72c1f612f0273
aghounam <ahmedghounami0@gmail.com>
2025-10-22 17:48:37 +0100

    ...

--- server/server.js
+++ server/server.js
@@ -30,7 +30,9 @@ db.serialize(() => {
 	  CREATE TABLE IF NOT EXISTS users (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		name TEXT NOT NULL,
-		picture TEXT NOT NULL,
+		email TEXT UNIQUE,
+		password TEXT,
+		picture TEXT,
 		gold INTEGER DEFAULT 0,
 		created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 	  );
